f11fb1964bb108e3a8cc90d29609d5ce8bb8fea7
zangqingan <18367319791@163.com>
2024-06-06 11:45:27 +0800

    feat:去除不必要dto文件

--- server/src/modules/monitor/login-log/dto/update-loginlog.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsNumber } from 'class-validator';
-import { PartialType } from '@nestjs/swagger';
-import { CreateLoginLogDto } from './create-loginlog.dto';
-
-export class UpdateLoginLogDto extends PartialType(CreateLoginLogDto) {
-  @IsNumber()
-  infoId: number;
-}
